38ee3ea0ef209caa80ffd8885396ddddcd90b15a
ForsyteRun <forsyterun@gmail.com>
2023-03-10 14:51:06 +0200

    feat: add full hover tasc(Fri, 10 Mar, 2023, 14:50:25)

--- cssBayan/index.js
+++ cssBayan/index.js
@@ -6,25 +6,39 @@ accordionBlock.forEach((block) => {
 
     //transform cross by hover
     const blockItem = block.querySelector('.fa-solid')
-    const blockTitle = block.querySelector('.accordion__title')
+    const blockTitle = document.querySelectorAll('.accordion__title')
     blockItem.style.cssText = "transform: rotate(45deg); transition: all 0.4s ease-in-out;"
-    blockTitle.style.cssText = "transition: all 0.4s ease-in-out; color: #000"
+    blockTitle.forEach((title) => {
+      title.style.cssText = "transition: all 0.4s ease-in-out; color: #000"
+    })
+
     //cross on
     icons.forEach((icon) => {
       icon.classList.add('visibl')
     })
+
+    //appear img
+    const img = block.querySelector('.accordion__img img')
+    img.style.cssText = "height: 30rem; width: 60% width: 80%; transition: all 0.4s ease"
+
   })
   block.addEventListener('mouseleave', () => {
 
     //transform cross by hover
     const blockItem = block.querySelector('.fa-solid')
-    const blockTitle = block.querySelector('.accordion__title')
-    blockItem.style.cssText = "transform: rotate(-90deg); transition: all 0.4s ease-in-out;"
-    blockTitle.style.cssText = "transition: all 0.4s ease-in-out; color: #1b1ba8ea"
+    const blockTitle = document.querySelectorAll('.accordion__title')
+    blockItem.style.cssText = "transform: rotate(-90deg); transition: all 0.1s ease;"
+    blockTitle.forEach((title) => {
+      title.style.cssText = "transition: all 0.4s ease-in-out; color: #1b1ba8ea"
+    })
 
     //cross off
     icons.forEach((icon) => {
       icon.classList.remove('visibl')
     })
+
+    //disappear img
+    const img = block.querySelector('.accordion__img img')
+    img.style.cssText = "height: 0; width: 0; transition: all 0.7s ease-in-out;"
   })
 })
